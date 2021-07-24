d128093672db0dd213b3dced8dc20c068e06717f
Anisa Sinanaj <sinani_ana1991@hotmail.com>
2021-07-24 12:05:15 +0200

    last update of template

--- src/main/java/com/example/auction/controllers/AuctionController.java
+++ src/main/java/com/example/auction/controllers/AuctionController.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AboutController {
-  @GetMapping("/about")
-  public String about()
-  {return "about";}
+public class AuctionController {
+    @GetMapping("/auction")
+    public String getBuy(){
+        return "auctions";
+
+    }
+
 }
