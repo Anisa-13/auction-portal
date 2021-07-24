44516827c430b95671dbe6cfa8c16a236fd85468
fatjona.hysa@fti.edu.al <fatjona.hysa@fti.edu.al>
2021-07-24 16:34:02 +0200

    add auction model

--- src/main/java/com/example/auction/model/Auction.java
+++ src/main/java/com/example/auction/model/Auction.java
@@ -1,4 +1,105 @@
 package com.example.auction.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+@Entity
 public class Auction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer auctionId;
+
+    @NotEmpty
+    private String auctionPhoto;
+
+    @NotEmpty
+    private String productName;
+
+    @NotEmpty
+    private String description;
+
+    @NotEmpty
+    private Integer startPrice;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private boolean isConfirmed;
+
+    public Auction() {
+    }
+
+    public Auction(Integer auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public Integer getAuctionId() {
+        return auctionId;
+    }
+
+    public void setAuctionId(Integer auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public String getAuctionPhoto() {
+        return auctionPhoto;
+    }
+
+    public void setAuctionPhoto(String auctionPhoto) {
+        this.auctionPhoto = auctionPhoto;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getStartPrice() {
+        return startPrice;
+    }
+
+    public void setStartPrice(Integer startPrice) {
+        this.startPrice = startPrice;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isConfirmed() {
+        return isConfirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        isConfirmed = confirmed;
+    }
 }
